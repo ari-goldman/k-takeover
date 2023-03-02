5e9796c65a183c9da93756329422ad6b8908cebe
ari-goldman <112101394+ari-goldman@users.noreply.github.com>
2023-03-01 21:06:04 -0700

    Init

---
+++ src/main/java/me/argo/ktakeover/KTakeover.kt
@@ -0,0 +1,14 @@
+package me.argo.ktakeover
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class KTakeover : JavaPlugin() {
+    // F5 to build!
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
